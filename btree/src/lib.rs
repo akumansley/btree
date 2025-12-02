759dfeb98b0f0e4818b112044c81ba3a25e34d23
Andrew Kumansley <andrew.kumansley@airtable.com>
2025-12-02 20:17:48 +0000

    Publish enums supporting remove_or_modify_if

--- btree/src/lib.rs
+++ btree/src/lib.rs
@@ -37,4 +37,7 @@ pub use cursor::{Cursor, CursorMut};
 pub use iter::{BackwardBTreeIterator, ForwardBTreeIterator};
 pub use qsbr::{qsbr_reclaimer, MemoryReclaimer};
 pub use reference::{Entry, Ref};
-pub use tree::{BTree, BTreeKey, BTreeValue, InsertOrModifyIfResult};
+pub use tree::{
+    BTree, BTreeKey, BTreeValue, InsertOrModifyIfResult, RemoveOrModifyDecision,
+    RemoveOrModifyIfResult,
+};
